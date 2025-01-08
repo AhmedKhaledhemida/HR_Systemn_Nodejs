a8680944d2d27edcd6d980fab23e22bc64855007
Ahmed Khaled <ahmedkhaledhemida1@gmail.com>
2025-01-09 01:07:49 +0200

    add

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ app.use(globalError);
 process.on("unhandledRejection", (err) => {
   console.log({ error: err });
 });
-
+app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
